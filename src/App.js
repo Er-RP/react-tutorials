dc5badbddc6d7dcf7cb1b818ef96804a84ac1dd7
Rajapandi <92912777+Rajapandi1212@users.noreply.github.com>
2022-07-10 00:03:01 +0530

    Simple Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
 const App = () => {
-  return <div>hiii</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="products" element={<Products />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
 };
 
 export default App;
