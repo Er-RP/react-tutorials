d3dab209bfef6f88afc6592ac9362d6d57ecfb95
Rajapandi <92912777+Rajapandi1212@users.noreply.github.com>
2022-07-19 22:51:49 +0530

    useState basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  /* Initializing useState . useState always return array which is combination of  currentState [count],
+  function [setCount] to change currentState . useState accepts one initialValue [0]*/
+  const [count, setCount] = useState(0);
+  //Function to change state
+  const handleIncreaseCount = () => setCount((prevCount) => prevCount + 1);
+  const handleDecreaseCount = () => setCount((prevCount) => prevCount - 1);
+
   return (
     <>
-      <div className="App">Welcome to react hooks</div>
+      <div className="App">
+        <button onClick={() => handleIncreaseCount()}>+</button>
+        <p>{count}</p>
+        <button onClick={() => handleDecreaseCount()}>-</button>
+      </div>
     </>
   );
-}
+};
 
 export default App;
